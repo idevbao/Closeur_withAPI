f71422d52471dc3679711cbdef645688a469c51b
trucsphuongw <osxvn@trucphuong.local>
2018-03-02 08:20:13 +0700

    Initial Commit

---
+++ Closeur_withAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Closeur_withAPI
+//
+//  Created by Trúc Phương >_< on 02/03/2018.
+//  Copyright © 2018 iDev Bao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Closeur_withAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Closeur_withAPI
+//
+//  Created by Trúc Phương >_< on 02/03/2018.
+//  Copyright © 2018 iDev Bao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
